5adfa112206589189c0d2271bb936aded576c4e5
tombakke-civitas <81184364+tombakke-civitas@users.noreply.github.com>
2025-04-13 17:01:20 -0600

    adding template for housing_app.py

---
+++ housing_app.py
@@ -0,0 +1,9 @@
+import duckdb
+import pandas as pd 
+import streamlit as st
+
+# df = pd.read_csv("regional-global-weekly-2025-03-27.csv")
+
+# con = duckdb.connect("spotify_charts.db")
+# con.execute("CREATE TABLE IF NOT EXISTS charts AS SELECT * FROM df")
+# con.close()
